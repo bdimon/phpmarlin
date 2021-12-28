a7f365a940c58b363f721d9d123b708e13652014
bdimon <regsdimon-reg@yahoo.com>
2021-12-28 20:34:40 +0200

    Update and rename insert_txt.php to txt_ins.php

--- txt_ins.php
+++ txt_ins.php
@@ -2,8 +2,8 @@
 // var_dump($_POST);
 $text = $_POST["text"];
 $pdo = new PDO("mysql:host=localhost;dbname=marlin;", "root", "" );
-$sql = "INSERT  INTO text (content) VALUES (:txt)";
-$stmt = $pdo->prepare($sql);
-$stmt->execute(['txt' => $text]);
+$sql = "INSERT  INTO text (content) VALUES ('$text')";
+$statement = $pdo->prepare($sql);
+$statement->execute();
 header('Location: /task_9.html');
 ?>
